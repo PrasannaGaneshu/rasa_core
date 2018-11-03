0f7cc532ac3bfdb853ab590cb095b33bd454006c
Nathan Zylbersztejn <nathan@mrbot.ai>
2018-11-03 13:31:55 -0400

    removed debug statements

--- rasa_core/run.py
+++ rasa_core/run.py
@@ -226,9 +226,7 @@ def serve_application(initial_agent,
 def load_agent(core_model, interpreter, endpoints,
                tracker_store=None,
                wait_time_between_pulls=100):
-
     if endpoints.model:
-        print(endpoints.model)
         return agent.load_from_server(
                 interpreter=interpreter,
                 generator=endpoints.nlg,
